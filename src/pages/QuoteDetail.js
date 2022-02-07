79959c307b826eb9467b5695dc3fea457e80d9fa
Mark Atherton <mark@insightinternet.co.uk>
2022-02-07 17:44:57 +0000

    imported <Comments /> and used as Route's child

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,12 +1,16 @@
 import { useParams, Route } from "react-router-dom";
 
+import Comments from "../components/comments/Comments";
+
 const QuoteDetail = () => {
   const params = useParams();
   return (
     <>
       <h1>Quote Detail page</h1>
       <p>{params.quoteId}</p>
-      <Route path={`/quotes/${params.quoteId}/comments`}></Route>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
     </>
   );
 };
