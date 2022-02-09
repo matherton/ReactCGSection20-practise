4e5fe8ae0a837027269235bffcdb55fea456cc06
Mark Atherton <mark@insightinternet.co.uk>
2022-02-09 10:52:59 +0000

    imported QuoteForm and created console.log func and passed it as prop to <QuoteForm />

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,5 +1,15 @@
+import QuoteForm from "../components/quotes/QuoteForm";
+
 const NewQuote = () => {
-  return <h1>New Quote Page</h1>;
+  const addQuoteHandler = (quoteData) => {
+    console.log(quoteData);
+  };
+  return (
+    <>
+      <h1>New Quote Page</h1>
+      <QuoteForm onAddQuote={addQuoteHandler} />
+    </>
+  );
 };
 
 export default NewQuote;
