6f3e057d2682c14ab8395da846045fabfd8520df
Mark Atherton <mark@insightinternet.co.uk>
2022-02-09 12:48:23 +0000

    imported useHistory and stored as history object

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,6 +1,9 @@
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
   };
