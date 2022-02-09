12d52149baaf894b450edc80d3ccec9544845be6
Mark Atherton <mark@insightinternet.co.uk>
2022-02-09 11:27:33 +0000

    pulled out current quote form DUMMY_DATA and stored as const quote

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -2,8 +2,23 @@ import { useParams, Route } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
 
+const DUMMY_DATA = [
+  {
+    id: "p1",
+    author: "Mark",
+    text: "Learning React is fun!",
+  },
+  {
+    id: "p2",
+    author: "Matthew",
+    text: "Learning to ride a bike is fun!",
+  },
+];
+
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_DATA.find((quote) => quote.id === params.quoteId);
   return (
     <>
       <h1>Quote Detail page</h1>
