2d518828c36801557d9296570c68f4aaba303702
Mark Atherton <mark@insightinternet.co.uk>
2022-02-14 20:30:58 +0000

    compared it to asc string to return a boolean

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -8,7 +8,7 @@ const QuoteList = (props) => {
   //built in browser constructor function
   const queryParams = new URLSearchParams(location.search);
 
-  const isSortingAscending = queryParams.get("sort");
+  const isSortingAscending = queryParams.get("sort") === "asc";
 
   const changeSortingHandler = () => {
     history.push(`/quotes?asc`);
