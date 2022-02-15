2fbb336720d97da79f1376a373a01ff262fa71cc
Mark Atherton <mark@insightinternet.co.uk>
2022-02-15 12:26:33 +0000

    end of lesson 286: made button dynmic text and url params update accordingly

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,18 +1,32 @@
 import { useHistory, useLocation } from "react-router-dom";
+
 import QuoteItem from "./QuoteItem";
 import classes from "./QuoteList.module.css";
+
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
 
 const QuoteList = (props) => {
   const history = useHistory();
   const location = useLocation();
-  //built in browser constructor function
+
   const queryParams = new URLSearchParams(location.search);
 
   const isSortingAscending = queryParams.get("sort") === "asc";
 
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
   const changeSortingHandler = () => {
-    history.push(`/quotes?${isSortingAscending ? "desc" : "asc"}`);
+    history.push("/quotes?sort=" + (isSortingAscending ? "desc" : "asc"));
   };
+
   return (
     <>
       <div className={classes.sorting}>
@@ -21,7 +35,7 @@ const QuoteList = (props) => {
         </button>
       </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
