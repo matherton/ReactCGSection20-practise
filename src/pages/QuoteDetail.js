eaa2f634338b9ec6876650766172410d2fc7fdb1
Mark Atherton <mark@insightinternet.co.uk>
2022-02-15 13:29:33 +0000

    end of lesson 287: added exact Route path to only show the load comments link when on path without \comments at the end

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -28,11 +28,13 @@ const QuoteDetail = () => {
   return (
     <>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <div className="centered">
-        <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
-          Load Comments
-        </Link>
-      </div>
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
