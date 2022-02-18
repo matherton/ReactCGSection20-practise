ec14d749dffd4f5b4e86b44ea8a827848d01c9ce
Mark Atherton <mark@insightinternet.co.uk>
2022-02-18 12:48:29 +0000

    refactored history.push to receive object

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -24,9 +24,10 @@ const QuoteList = (props) => {
   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
 
   const changeSortingHandler = () => {
-    history.push(
-      `${location.pathname}?sort=` + (isSortingAscending ? "desc" : "asc")
-    );
+    history.push({
+      pathname: location.pathname,
+      search: `?sort=` + (isSortingAscending ? "desc" : "asc"),
+    });
   };
 
   return (
