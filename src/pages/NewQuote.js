6d440fde8435b50a7d907476295e97d34966558e
Mark Atherton <mark@insightinternet.co.uk>
2022-02-18 12:59:15 +0000

    added Max's useHttp hook and called it in NewQuote function

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,10 @@
 import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
+import useHttp from "../components/hooks/use-http";
 
 const NewQuote = () => {
   const history = useHistory();
+  useHttp();
 
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
