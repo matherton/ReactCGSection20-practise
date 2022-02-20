6b4c877b9b46b2dcb80aea9eaac21db2589deee9
Mark Atherton <mark@insightinternet.co.uk>
2022-02-20 13:02:43 +0000

    removed error from useHttp(getAllComments) extraction as just focusing on core functionality

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -10,12 +10,7 @@ const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
   const params = useParams();
 
-  const {
-    sendRequest,
-    status,
-    data: loadedComments,
-    error,
-  } = useHttp(getAllComments);
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
