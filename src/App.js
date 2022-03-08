140d850fa7ffa917d5d69c0137b7807850239e59
Mark Atherton <mark@insightinternet.co.uk>
2022-03-08 15:30:37 +0000

    end of lesson 295: moved all component imports into lazy loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 
-import AllQuotes from "./pages/AllQuotes";
-import QuoteDetail from "./pages/QuoteDetail";
 import Layout from "./components/layout/Layout";
-import NotFound from "./pages/NotFound";
 import LoadingSpinner from "./components/UI/LoadingSpinner";
 
 const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
+const AllQuotes = React.lazy(() => import("./pages/AllQuotes"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
 
 function App() {
   return (
